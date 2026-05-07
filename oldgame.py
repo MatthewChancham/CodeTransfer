86fd4c45e704cf57f109f7cc119e9ca2daa5d166
JF-1-SVC <22FauJ@svcpupils.org>
2026-05-07 13:55:01 +0100

    Update and rename oldgame.py to main.py

--- oldgame.py
+++
@@ -1,9 +0,0 @@
-"""Entry point for the RPG game.
-This file now delegates to the modular implementation in ui.py.
-"""
-
-from ui import MainApp
-
-if __name__ == '__main__':
-    app = MainApp()
-    app.mainloop()
